ae44ad09ab27190bfdd797f862ad8c68d216d09e
tungrg <tungrg07@gmail.com>
2025-04-04 08:35:07 +0700

    part 2

--- Assets/Script/MusicController.cs
+++ Assets/Script/MusicController.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,32 +6,47 @@ public class MusicController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Slider musicSlider;
+    public Toggle musicToggle;
     void Start()
     {
+        //Neu da luu lai roi
         if (PlayerPrefs.HasKey("volBucket"))
         {
             LoadVolume();
-            musicSlider.value = PlayerPrefs.GetFloat("volBucket");
         }
+        //Neu chua thi cho am luong la 50%
         else
         {
             PlayerPrefs.SetFloat("volBucket", 0.5f);
-            musicSlider.value = PlayerPrefs.GetFloat("volBucket");
             LoadVolume();
         }
     }
+    //thay doi volume khi chay game (runtime)
     public void SetVolume()
     {
         AudioListener.volume = musicSlider.value;
+        SaveVolume();
     }
+    //Luu gia tri volume lai
     public void SaveVolume()
     {
         PlayerPrefs.SetFloat("volBucket", musicSlider.value);
-        PlayerPrefs.Save();
     }
+    //Lay gia tri volume ra
     public void LoadVolume()
     {
-        PlayerPrefs.SetFloat("volBucket", musicSlider.value);
+        musicSlider.value = PlayerPrefs.GetFloat("volBucket");
+        SetVolume();
+    }
+    public void MuteVolume()
+    {
+        if (musicToggle.isOn) {
+            AudioListener.volume = 0;
+        }
+        else
+        {
+            SetVolume();
+        }
     }
     // Update is called once per frame
     void Update()
